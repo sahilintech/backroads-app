bc922b8fb4cce2c116ab71cbea6abf8b50bf0e80
Sahil <sahilchauhan45448@gmail.com>
2023-02-02 20:27:35 +0530

    add the app

---
+++ backroads-app/src/App.js
@@ -0,0 +1,21 @@
+import Navbar from './Components/Navbar';
+import Hero from './Components/Hero';
+import About from './Components/About';
+import Services from './Components/Services';
+import Tours from './Components/Tours';
+import Footer from './Components/Footer';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Hero />
+    <About />
+    <Services />
+    <Tours />
+    <Footer />
+    </>
+  )
+}
+
+export default App;
